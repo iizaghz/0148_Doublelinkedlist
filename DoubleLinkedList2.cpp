142bb8ddcdb0cce7391087af7338775f3a12bd2c
iizaghz <sukmahawaiza@gmail.com>
2025-05-15 17:35:42 +0700

    add fungsi search data

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -176,4 +176,18 @@ class DoubleLinkedList
             i--;
         }
     }
+
+    void searchData()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+        int rollNo;
+        cout << "\nEnter the roll number to search:";
+        cin >> rollNo;
+
+        Node *current = START;
+    }
 };
