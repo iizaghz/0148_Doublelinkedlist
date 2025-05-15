22cb4d0b864f02368d2519701a21dad750f8bedb
iizaghz <sukmahawaiza@gmail.com>
2025-05-15 17:31:14 +0700

    add  step 2 raverse backward

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -164,5 +164,12 @@ class DoubleLinkedList
             currentNode = currentNode->next;
             i++;
         }
+
+        //step 2 : traverse backward
+        cout << "\nRecords in descending order of roll number are:\n";
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+        }
     }
 };
