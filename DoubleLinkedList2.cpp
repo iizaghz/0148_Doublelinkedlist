e4c4e765ed3005706a043e66c62d40c5a1d881ef
iizaghz <sukmahawaiza@gmail.com>
2025-05-15 16:51:12 +0700

    add insert last node

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -73,7 +73,10 @@ class DoubleLinkedList
             newNode->next = current->next;  //step 9a : newNode.next = current.next
             newNode->prev = current;        //step 9b : newNode.prev = current
 
-            
+            // insert last node
+            if (current->next !=NULL)
+                current->next->prev = newNode;  // step 9c : current.next.prev
+                current->next = newNode;        // step 9d : current.next = newNode
     }
     
 };
