8d8d39276aa1c3ab5cf92be247ea1194b84ca93c
iizaghz <sukmahawaiza@gmail.com>
2025-05-15 15:21:30 +0700

    add step 2

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -29,6 +29,10 @@ class DoubleLinkedList
 
         //step 1 : Allocate memory for new node
         Node *newNode =new Node();
+
+        // step 2 : Assign value to the data fields
+        newNode->noMhs = nim;
+        
     }
     
 };
