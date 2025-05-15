1c377acd8e57f3c1f25cb2e2ad84a6df63a52bcf
iizaghz <sukmahawaiza@gmail.com>
2025-05-15 16:50:24 +0700

    add step 9 insert between current and current->next

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -69,7 +69,11 @@ class DoubleLinkedList
                 cout << "\nDuplicate roll numbers not allowed" << endl;
                 return;
             }
-           
+            //step 9: insert between current and current->next
+            newNode->next = current->next;  //step 9a : newNode.next = current.next
+            newNode->prev = current;        //step 9b : newNode.prev = current
+
+            
     }
     
 };
