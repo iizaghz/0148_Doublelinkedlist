4e67cc930e530e07189d64e6d76998f61554be25
iizaghz <sukmahawaiza@gmail.com>
2025-05-15 16:45:08 +0700

    add step 5 STAR.prev = newNode (if START exists

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -44,6 +44,10 @@ class DoubleLinkedList
             // step 4 : newNode.next = START
             newNode->next = START;
 
+            // step 5 : START.prev = newNode (if START exists)
+            if (START != NULL)
+                START->prev - newNode;
+
         }
     }
     
