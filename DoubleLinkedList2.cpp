46531120f641da2012b18bc8454fec6f4ade0946
iizaghz <sukmahawaiza@gmail.com>
2025-05-15 17:00:42 +0700

    add step 4 if currrebt is not the last node

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -113,7 +113,12 @@ class DoubleLinkedList
             // step 3 : link previous node to next of  current
             current->prev->next = current->next;
 
+            //step 4 : if current is not the last node  
+            if (current->next != NULL)
+                current->next->prev = current->prev;
         }
+
+       
     }
     
 };
